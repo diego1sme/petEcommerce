f5cf0a97a73886377b17a57a6723966018bb3fc6
NabeelDhukka <Nabeeldhukka@gmail.com>
2018-03-22 11:55:23 -0500

    updated home url

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from homepage import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    url(r'home.html$', views.HomePageView.as_view()),
     url(r'login.html',views.Login.as_view()),
     url(r'contact_Us.html',views.contact_Us.as_view()),
     url(r'about_Us.html',views.about_Us.as_view()),
